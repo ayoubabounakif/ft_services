73ebb5814f60de9d9e0661012d0a0a211ae2cbda
Ayoub Abounakif <aabounak@e2r2p2.1337.ma>
2021-01-25 18:09:32 +0100

    Working

--- orchestration.sh
+++ orchestration.sh
@@ -6,9 +6,9 @@ docker build -t local-nginx srcs/Nginx/
 docker build -t local-mysql srcs/MySQL/
 docker build -t local-wordpress srcs/WordPress/
 docker build -t local-phpmyadmin srcs/phpMyAdmin/
-# docker build -t local-influxdb srcs/InfluxDB/
-# docker build -t local-grafana srcs/Grafana/
-# docker build -t local-ftps srcs/FTPS/
+docker build -t local-influxdb srcs/InfluxDB/
+docker build -t local-grafana srcs/Grafana/
+docker build -t local-ftps srcs/FTPS/
 
 # 1 - MetalLB Installation
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
@@ -20,14 +20,14 @@ sed -i '' "s/192.168.99.*/$minikubeip-$minikubeip/g" ./srcs/metallb-configmap.ya
 
 kubectl apply -f srcs/metallb-configmap.yaml
 
-# 2 - Nginx / MySQL / Wordpress
+# 2 - Nginx / MySQL / WordPress
 kubectl apply -f srcs/nginx-deplsvc.yaml
 kubectl apply -f srcs/mysql-deplsvc.yaml
 kubectl apply -f srcs/wordpress-deplsvc.yaml
 kubectl apply -f srcs/phpmyadmin-deplsvc.yaml
-# kubectl apply -f srcs/influxdb-deplsvc.yaml
-# kubectl apply -f srcs/grafana-deplsvc.yaml
-# kubectl apply -f srcs/ftps-deplsvc.yaml
+kubectl apply -f srcs/influxdb-deplsvc.yaml
+kubectl apply -f srcs/grafana-deplsvc.yaml
+kubectl apply -f srcs/ftps-deplsvc.yaml
 
 kubectl get pods
 minikube start
