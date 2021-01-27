51a2d864c17e9056cb6954bb80cd32da932e58cf
Ayoub Abounakif <aabounak@e2r2p2.1337.ma>
2021-01-27 16:34:14 +0100

    CSS WP | SSH Nginx | (Need to test FTPS)

--- srcs/Nginx/nginx-script.sh
+++ srcs/Nginx/nginx-script.sh
@@ -1,9 +1,10 @@
 sudo openrc reboot
 
 pkill nginx
-rc-service nginx start
 
-# /telegraf-1.17.0/usr/bin/telegraf --config /etc/telegraf/telegraf.conf &
+rc-service nginx start
+/usr/sbin/sshd -D &
+/telegraf-1.17.0/usr/bin/telegraf --config /etc/telegraf/telegraf.conf
 
 # sh
 tail -F /dev/null
