2236372bededcaf9ec8b3b573fc221cf37f91925
Ayoub Abounakif <aabounak@e2r2p2.1337.ma>
2021-01-27 09:54:27 +0100

    WP

--- orchestration.sh
+++ orchestration.sh
@@ -15,9 +15,6 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manife
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
-minikubeip=$(minikube ip)
-sed -i '' "s/192.168.99.*/$minikubeip-$minikubeip/g" ./srcs/metallb-configmap.yaml
-
 kubectl apply -f srcs/metallb-configmap.yaml
 
 # 2 - Nginx / MySQL / WordPress
--- srcs/WordPress/wp-config.php
+++ srcs/WordPress/wp-config.php
@@ -46,14 +46,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define('AUTH_KEY',         '*=z+UF1X-7p(pvP+Z(F,#&y*QJV$=}!g^k.x8ceo0*_|4#3>03cP*s.ocU|>jUAx');
+define('SECURE_AUTH_KEY',  ']_KGA)>HS29)D-oZ`]}- X%<_jz|pE`vG64D!X]++yP,?I1Z/](Lh&|C@oZz>8g^');
+define('LOGGED_IN_KEY',    'SYac {*e:]qhDEe|JiYv#VggG(6N&P1E58K#{]szN,B)/NLOF.rE`/;}+meNf)yh');
+define('NONCE_KEY',        'vM:3^bA&c7~<-t4VpXAOmy>-=@kWf .uw,vX~I3hWv*F|((</bO<-HA,y+8}t%(}');
+define('AUTH_SALT',        '+5=d:`s]=O4m)<y*]+6_.T++=GTv<#-$L19bkO)>[#`!X:!$w7e9~Smvb3LTW%O-');
+define('SECURE_AUTH_SALT', 'cQ9y`)WmDKc7{x(g@u{-E-7 ~-]6$a({fSp :.TNX4mE~T#6.+H@u[5N( Vb-H9v');
+define('LOGGED_IN_SALT',   '|QJ_T&rj4`VJ7@+yILS0Hs=LKD5D@8tOnY7]S>nx|+ID+8MMn*.@A`NB*}xHY a-');
+define('NONCE_SALT',       '.wcSRY<^UPHG2?69v|6s*0H[M]8WjQogV6)iAHz59P7sungz}pu9}Ktok{*?Y5QZ');
 
 /**#@-*/
 
@@ -78,6 +78,9 @@ $table_prefix = 'wp_';
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
 define( 'WP_DEBUG', false );
+
+# Fixes CSS problems
+define('CONCATENATE_SCRIPTS', false);
 
 /* That's all, stop editing! Happy publishing. */
 
